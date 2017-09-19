fc4686c1004fc8cd544eea4a317fff0034f1facb
Eric Schwartz <eric.schwartz@theironyard.com>
2017-09-19 12:42:10 -0700

    working with one COMponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,34 @@ import {JeopardyService } from './jeopardy.service';
 export class AppComponent implements OnInit {
   title = 'app';
 
+  userScore: number = 0;
+  userAnswer: string;
+
   questionInfo;
 
   constructor(private jeopardyService: JeopardyService){}
 
+  //method that uses the jeopardy service from the api
   getDataFromService(){
     this.jeopardyService.getQuestionInfo()
       .subscribe(
         questionInfo => {
           this.questionInfo = questionInfo[0];
+          console.log(this.questionInfo.answer)
         }
       )
   }
 
+  checkAnswer(){
+    if(this.userAnswer == this.questionInfo.answer){
+      this.userScore += this.questionInfo.value;
+    }
+    this.userAnswer = "";
+    this.getDataFromService();
+  }
+
+  //call the api on loading of the component to get
+  //initial data
   ngOnInit(){
     this.getDataFromService()
   }
